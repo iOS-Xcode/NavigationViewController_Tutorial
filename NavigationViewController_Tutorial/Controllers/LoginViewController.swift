101ae1bee85d9aeca1d2ff8485dd74dd7984d1c0
Seokhyun Kim <wjapp72@gmail.com>
2020-11-02 15:15:21 -0800

    Initial Commit

---
+++ NavigationViewController_Tutorial/Controllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  NavigationViewController_Tutorial
+//
+//  Created by Seokhyun Kim on 2020-11-02.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Navigation Bar hidden
+        self.navigationController?.isNavigationBarHidden = true
+    }
+}
